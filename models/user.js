3e6191790cb19aa7251d5b70e0384123d7d92a73
khoaHyh <khoahuynh.toronto@gmail.com>
2021-03-14 14:41:18 -0400

    Initial commit

---
+++ models/user.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  username: { type: String, required: true, index: { unique: true } },
+  password: { type: String, required: true },
+  joined: { type: Date, default: new Date() },
+  provider: String,
+});
+
+module.exports = mongoose.model("user", userSchema);
