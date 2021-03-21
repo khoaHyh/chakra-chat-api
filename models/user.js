6a5093129d25faa57aa07092848446696a8266c5
khoaHyh <khoahuynh.toronto@gmail.com>
2021-03-21 18:03:54 -0400

    Fix: Restraints in user model

--- models/user.js
+++ models/user.js
@@ -1,9 +1,9 @@
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-  email: { type: String, required: true, index: { unique: true } },
+  email: { type: String, index: { unique: true } },
   username: { type: String, required: true, index: { unique: true } },
-  password: { type: String, required: true },
+  password: { type: String },
   joined: { type: Date, default: new Date() },
   provider: String,
   active: { type: Boolean, default: false },
