c09256252af1e2983297633ea1bfc562acd98f71
khoaHyh <khoahuynh.toronto@gmail.com>
2021-04-09 23:18:51 -0400

    Set useFindAndModify to false

--- utilities/db.js
+++ utilities/db.js
@@ -1,16 +1,17 @@
-require('dotenv').config();
-const mongoose = require('mongoose');
+require("dotenv").config();
+const mongoose = require("mongoose");
 
 const connectDB = async () => {
-    try {
-        await mongoose.connect(process.env.MONGO_URI, { 
-            useCreateIndex: true,
-            useNewUrlParser: true, 
-            useUnifiedTopology: true 
-        });
-    } catch (error) {
-        console.error(`Initial database connection error: ${error}`);
-    }
-}
+  try {
+    await mongoose.connect(process.env.MONGO_URI, {
+      useCreateIndex: true,
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false,
+    });
+  } catch (error) {
+    console.error(`Initial database connection error: ${error}`);
+  }
+};
 
 module.exports = connectDB;
