4d03732ab59aba2f974391785841999d6e52c2f0
khoaHyh <khoahuynh.toronto@gmail.com>
2021-05-14 20:20:30 -0400

    Try fixing github OAuth by reverting deserialization

--- auth.js
+++ auth.js
@@ -2,24 +2,31 @@ require("dotenv").config();
 const LocalStrategy = require("passport-local");
 const bcrypt = require("bcrypt");
 const GitHubStrategy = require("passport-github2").Strategy;
-const mongoose = require("mongoose");
 const User = require("./models/user");
 
 module.exports = (passport) => {
   // Convert object contents into a key
   passport.serializeUser((user, done) => done(null, user._id));
+
   // Convert key into original object and retrieve object contents
-  passport.deserializeUser(async (id, done) => {
-    try {
-      console.log("id:", id);
-      let user = await User.findById(id);
-      console.log("user:", user);
-      if (!user) return done(null, false, { message: "user not found" });
-      done(null, user);
-    } catch (error) {
-      done(error);
-    }
+  passport.deserializeUser((id, done) => {
+    User.findById(id, (err, user) => {
+      if (err) return done(error);
+      done(err, user);
+    });
   });
+
+  //passport.deserializeUser(async (id, done) => {
+  //  try {
+  //    console.log("id:", id);
+  //    let user = await User.findById(id);
+  //    console.log("user:", user);
+  //    if (!user) return done(null, false, { message: "user not found" });
+  //    done(null, user);
+  //  } catch (error) {
+  //    done(error);
+  //  }
+  //});
 
   // Define process to use when we try to authenticate someone locally
   passport.use(
